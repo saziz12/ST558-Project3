530c3f6ab311b424ac243eca60eb445fe14eee25
saziz12 <saaziz0522@gmail.com>
2023-07-31 18:56:18 -0400

    model fitting

--- App/server.R
+++ App/server.R
@@ -13,6 +13,8 @@ titanic$survived1 <- as.character(titanic$survived)
 titanic$sex1 <- as.character(titanic$sex) 
 titanic$embarked1 <- as.character(titanic$embarked) 
 
+titanic_clean <- titanic[complete.cases(titanic), ]
+
 # Server
 shinyServer(function(input, output, session) {
   
@@ -100,6 +102,138 @@ shinyServer(function(input, output, session) {
       summaryStats()
     })
   })
+  
+  
+  # Modeling Page
+  # Model Fitting
+  observeEvent(input$fit_models_button, {
+    # Split the data into training and testing sets
+    set.seed(123) # For reproducibility
+    train_indices <- createDataPartition(titanic_clean$survived, p = input$train_test_split, list = FALSE)
+    train_data <- titanic_clean[train_indices, ]
+    test_data <- titanic_clean[-train_indices, ]
+    
+    # Fit the models based on user selection
+    model_fit_stats <- fit_models(train_data, test_data)
+    
+    # Store the model fit statistics for rendering
+    output$model_fit_stats <- renderPrint({
+      model_fit_stats
+    })
+  })
+  
+  # Function to fit models
+  fit_models <- function(train_data, test_data) {
+    model_fit_stats <- list()
+    
+    # Model 1: Multiple Linear Regression
+    if (input$fit_model_mlr) {
+      selected_vars_model1 <- input$selected_vars_model1
+      if (length(selected_vars_model1) > 0) {
+        lm_formula <- as.formula(paste("survived1 ~", paste(selected_vars_model1, collapse = " + ")))
+        model_fit <- lm(lm_formula, data = train_data)
+        predictions_train <- predict(model_fit, newdata = train_data)
+        predictions_test <- predict(model_fit, newdata = test_data)
+        rmse_train <- sqrt(mean((train_data$survived1 - predictions_train)^2))
+        rmse_test <- sqrt(mean((test_data$survived1 - predictions_test)^2))
+        model_fit_stats$model_mlr <- list(
+          model_type = "Multiple Linear Regression",
+          rmse_train = rmse_train,
+          rmse_test = rmse_test,
+          summary_stats_train = summary(model_fit)
+        )
+      }
+    }
+    
+    # Model 2: Regression Tree
+    if (input$fit_model_tree) {
+      selected_vars_model2 <- input$selected_vars_model2
+      if (length(selected_vars_model2) > 0) {
+        formula <- as.formula(paste("survived1 ~", paste(selected_vars_model2, collapse = "+")))
+        model_fit <- rpart(formula, data = train_data, method = "class")
+        predictions_train <- predict(model_fit, newdata = train_data, type = "class")
+        predictions_test <- predict(model_fit, newdata = test_data, type = "class")
+        accuracy_train <- sum(diag(table(train_data$survived1, predictions_train))) / nrow(train_data)
+        accuracy_test <- sum(diag(table(test_data$survived1, predictions_test))) / nrow(test_data)
+        model_fit_stats$model_tree <- list(
+          model_type = "Regression Tree",
+          accuracy_train = accuracy_train,
+          accuracy_test = accuracy_test,
+          summary_stats_train = summary(model_fit)
+        )
+      }
+    }
+    
+    # Model 3: Random Forest
+    if (input$fit_model_rf) {
+      selected_vars_model3 <- input$selected_vars_model3
+      if (length(selected_vars_model3) > 0) {
+        formula <- as.formula(paste("survived1 ~", paste(selected_vars_model3, collapse = "+")))
+        model_fit <- randomForest(formula, data = train_data)
+        predictions_train <- predict(model_fit, newdata = train_data)
+        predictions_test <- predict(model_fit, newdata = test_data)
+        accuracy_train <- sum(predictions_train == train_data$survived1) / nrow(train_data)
+        accuracy_test <- sum(predictions_test == test_data$survived1) / nrow(test_data)
+        model_fit_stats$model_rf <- list(
+          model_type = "Random Forest",
+          accuracy_train = accuracy_train,
+          accuracy_test = accuracy_test,
+          variable_importance = importance(model_fit),
+          confusion_matrix_train = table(train_data$survived1, predictions_train),
+          confusion_matrix_test = table(test_data$survived1, predictions_test)
+        )
+      }
+    }
+    
+    return(model_fit_stats)
+  }
+  
+  
+  # Display fit statistics for each model
+  observeEvent(input$fit_models_button, {
+    output$model_fit_stats <- renderPrint({
+      results <- fit_models(train_data, test_data)
+      model_mlr <- results$model_mlr
+      model_tree <- results$model_tree
+      model_rf <- results$model_rf
+      
+      output_text <- ""
+      
+      if (!is.null(model_mlr)) {
+        output_text <- paste(output_text, "Model 1 -", model_mlr$model_type, "\n")
+        output_text <- paste(output_text, "Root Mean Squared Error (RMSE) - Training Set:", model_mlr$rmse_train, "\n")
+        output_text <- paste(output_text, "Root Mean Squared Error (RMSE) - Test Set:", model_mlr$rmse_test, "\n")
+        output_text <- paste(output_text, "Summary Statistics on Training Set:\n")
+        output_text <- paste(output_text, capture.output(model_mlr$summary_stats_train), collapse = "\n")
+        output_text <- paste(output_text, "\n\n")
+      }
+      if (!is.null(model_tree)) {
+        output_text <- paste(output_text, "Model 2 -", model_tree$model_type, "\n")
+        output_text <- paste(output_text, "Accuracy - Training Set:", model_tree$accuracy_train, "\n")
+        output_text <- paste(output_text, "Accuracy - Test Set:", model_tree$accuracy_test, "\n")
+        output_text <- paste(output_text, "Summary Statistics on Training Set:\n")
+        output_text <- paste(output_text, capture.output(model_tree$summary_stats_train), collapse = "\n")
+        output_text <- paste(output_text, "\n\n")
+      }
+      if (!is.null(model_rf)) {
+        output_text <- paste(output_text, "Model 3 -", model_rf$model_type, "\n")
+        output_text <- paste(output_text, "Accuracy - Training Set:", model_rf$accuracy_train, "\n")
+        output_text <- paste(output_text, "Accuracy - Test Set:", model_rf$accuracy_test, "\n")
+        output_text <- paste(output_text, "Variable Importance:\n")
+        output_text <- paste(output_text, "Variable Importance:\n")
+        output_text <- paste(output_text, capture.output(model_rf$variable_importance), collapse = "\n")
+        output_text <- paste(output_text, "\n\n")
+      }
+      
+      if (output_text == "") {
+        output_text <- "No models selected for fitting."
+      }
+      
+      cat(output_text)
+    })
+  })
+  
+  
   
   # Data Page
   # Display the Titanic dataset in a DataTable
--- App/ui.R
+++ App/ui.R
@@ -107,16 +107,14 @@ shinyUI(
                                  actionButton("runGraph", strong("Create Plot"))
                                  ),
                                mainPanel(
-                                 plotlyOutput("final_plot"),
-                                 # Table to display categorical variable summaries
-                                 dataTableOutput("categorical_summaries")
+                                 plotlyOutput("final_plot")
                                )
                              )
                     ), 
                     tabPanel("Numerical Summaries", 
                              sidebarLayout(
                                sidebarPanel(
-                                 h2("Select a Summary Type"),
+                                 h2("Select a Summary Variable"),
                                  radioButtons("sumvar", 
                                               "Select a Variable to Receive Summary Statistics:",
                                               c("Age" = "age",
@@ -138,29 +136,135 @@ shinyUI(
         
         # Modeling Page
         tabItem(tabName = "modeling",
-                # Add content 
                 # Three sub-tabs for Modeling Info, Model Fitting, and Prediction
                 tabsetPanel(
+                  # Modeling Info tab with model-specific sub-tabs
                   tabPanel("Modeling Info",
-                           # Add content
+                           tabsetPanel(
+                             # Sub-tab for Multiple Linear Regression / Generalized Linear Regression
+                             tabPanel("Multiple Linear Regression / Generalized Linear Regression", tabName = "model_info_lm",
+                                      fluidPage(
+                                        titlePanel("Multiple Linear Regression / Generalized Linear Regression"),
+                                        h4("Multiple linear regression is a statistical method used to model the relationship between a dependent variable and two or more independent variables. It assumes that the relationship between the dependent variable and the independent variables is linear. The model can be represented as:"),
+                                        h4(withMathJax(helpText("$$Y_i=\\beta_0+\\beta_1x_{1i}+\\beta_2x_{2i}+\\beta_3x_{1i}x_{2i}  +...+ E_i$$"))),
+                                        br(),
+                                        h4(strong("Benefits:")),
+                                        h4("1. MLR is simple and easy to interpret."),
+                                        h4("2. MLR can handle multiple predictors and can be extended to handle interactions between predictors."),
+                                        h4("3. MLR provides estimates of the coefficients, whcih helps the user understand the impact of each predictor on the dependent variable."),
+                                        br(),
+                                        h4(strong("Drawbacks:")),
+                                        h4("1. MLR assumes a linear relationship between the dependent and independent variables, which may not always be the case"),
+                                        h4("2. MLR is sensitive to outliers, which can significantly impact the model's performance"),
+                                        h4("3. MLR assumes the errors are independent and identically distributed, with may not always be the case.")
+                                      )
+                             ),
+                             
+                             # Sub-tab for Regression Tree
+                             tabPanel("Regression Tree",
+                                      fluidPage(
+                                        titlePanel("Regression Tree"),
+                                        # ... (Modeling Info for Regression Tree content)
+                                        # Example:
+                                        h4("Regression Tree:"),
+                                        p("A regression tree is a decision tree-based model used for regression tasks."),
+                                        p("It recursively partitions the data into subsets and fits a simple model (e.g., mean or median) to each subset."),
+                                        # ... (continue with explanations of regression tree)
+                                      )
+                             ),
+                             # Sub-tab for Random Forest
+                             tabPanel("Random Forest",
+                                      fluidPage(
+                                        titlePanel("Random Forest"),
+                                        # ... (Modeling Info for Random Forest content)
+                                        # Example:
+                                        h4("Random Forest:"),
+                                        p("Random Forest is an ensemble learning method that combines multiple decision trees."),
+                                        p("It improves the accuracy and reduces overfitting compared to a single decision tree."),
+                                        # ... (continue with explanations of random forest)
+                                      )
+                             )
+                           )
                   ),
                   
-                  tabPanel("Model Fitting",
-                           # Add content
+                  # Model Fitting tab
+                  tabPanel("Model Fitting", tabName = "model_fitting",
+                           fluidPage(
+                             h1("Model Fitting with Survival as the Dependent Variable"),
+                             h2("Train/Test Split"),
+                             sliderInput("train_test_split", "Proportion of Data for Training:", 
+                                         min = 0.5, max = 0.9, value = 0.7, step = 0.05),
+                             br(),
+                             br(),
+                             br(),
+                             h3("Select Variables for Model 1:"),
+                             checkboxGroupInput("selected_vars_model1", "Select Variables:",
+                                                c("Age" = "age",
+                                                  "Passenger Fare" = "fare",
+                                                  "Ticket Class" = "pclass1",
+                                                  "Sex" = "sex1",
+                                                  "Port of Embarkation" = "embarked1",
+                                                  "Number of Siblings/Spouses Aboard" = "sibsp",
+                                                  "Number of Parents/Children Aboard" = "parch"
+                                                )),
+                             checkboxInput("fit_model_mlr", "Fit Multiple Linear Regression"),
+                             br(),
+                             br(),
+                             br(),
+                             h3("Select Variables for Model 2:"),
+                             checkboxGroupInput("selected_vars_model2", "Select Variables:",
+                                                c("Age" = "age",
+                                                  "Passenger Fare" = "fare",
+                                                  "Ticket Class" = "pclass1",
+                                                  "Sex" = "sex1",
+                                                  "Port of Embarkation" = "embarked1",
+                                                  "Number of Siblings/Spouses Aboard" = "sibsp",
+                                                  "Number of Parents/Children Aboard" = "parch"
+                                                )),
+                             checkboxInput("fit_model_tree", "Fit Regression Tree"),
+                             br(),
+                             br(),
+                             br(),
+                             h3("Select Variables for Model 3:"),
+                             checkboxGroupInput("selected_vars_model3", "Select Variables:",
+                                                c("Age" = "age",
+                                                  "Passenger Fare" = "fare",
+                                                  "Ticket Class" = "pclass1",
+                                                  "Sex" = "sex1",
+                                                  "Port of Embarkation" = "embarked1",
+                                                  "Number of Siblings/Spouses Aboard" = "sibsp",
+                                                  "Number of Parents/Children Aboard" = "parch"
+                                                )),
+                             checkboxInput("fit_model_rf", "Fit Random Forest"),
+                             br(),
+                             br(),
+                             actionButton("fit_models_button", "Fit Models"),
+                             br(),
+                             br(),
+                             br(),
+                             h2("Model Fit Statistics:"),
+                             verbatimTextOutput("model_fit_stats")
+                           )
                   ),
                   
-                  tabPanel("Prediction",
-                           # Add content
+                  # Prediction tab
+                  tabPanel("Prediction", tabName = "prediction",
+                           fluidPage(
+                             titlePanel("Prediction"),
+                             # ... (Prediction content)
+                           )
                   )
                 )
         ),
+        
+        
         
         # Data Page
         tabItem(tabName = "data",
                 tabsetPanel(
                   tabPanel("Data",
                            fluidPage(
-                             titlePanel("Data"),
+                             h1("Data"),
                              br(),
                              # Display the Titanic dataset in a DataTable
                              DTOutput("titanic_table"),
@@ -168,10 +272,10 @@ shinyUI(
                              downloadButton("download_data", "Download Dataset")
                            )
                   ),
-                  tabPanel("Variable Information", 
+                  tabPanel("Variable Information",
                            fluidPage(
-                             titlePanel("Variable Information"),
-                             ## explain variables
+                             h1("Variable Information"),
+                             br(),
                              h2("Some Information about the Variables:"),
                              h4("survival: Survival (0 = No, 1 = Yes)"),
                              h4("pclass: Ticket Class (1 = 1st, 2 = 2nd, 3 = 3rd)"),
@@ -183,8 +287,7 @@ shinyUI(
                              h4("fare: Passenger Fare"),
                              h4("cabin: Cabin Number"),
                              h4("embarked: Port of Embarkation (C = Cherbourg, Q = Queenstown, S = Southampton)"),
-                             
-                             ## notes about the variables
+                             br(),
                              h2("Variable Notes"),
                              h4("pclass: A proxy for socio-economic status (SES)"),
                              h5("1st = Upper"),
